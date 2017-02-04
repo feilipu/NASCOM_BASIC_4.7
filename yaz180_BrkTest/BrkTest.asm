6a5e8917111d6cd62f7e5ab99f2fd38231389720
Phillip Stevens <phillip.stevens@gmail.com>
2017-02-04 21:38:14 +1100

    finished Single Step testing

--- yaz180_BrkTest/BrkTest.asm
+++ yaz180_BrkTest/BrkTest.asm
@@ -1,18 +1,8 @@
 
-IO_BASE         .EQU    $00     ; Internal I/O Base Address (ICR) <<< SET THIS AS DESIRED >>>
 
-; Operation Mode Control Reg (OMCR)
-OMCR            .EQU    IO_BASE+$3E     ; Operation Mode Control Reg
+WRKSPC  .EQU   $8000    ; Workspace for 32k Basic for yaz180
 
-OMCR_M1E        .EQU   $80    ; M1 Enable (0 Disabled)
-OMCR_M1TE       .EQU   $40    ; M1 Temporary Enable
-OMCR_IOC        .EQU   $20    ; IO Control (1 64180 Mode)
-
-        .org 3000H      ; start in 'X' jump, Basic prompt
-
-                        ; Set Operation Mode Control Reg (OMCR)
-        LD A,OMCR_M1E   ; Enable M1, but disable 64180 I/O _RD Mode
-        OUT0 (OMCR),A   ; X80 Mode (M1E Enabled, OIC Disabled)
+        .org 3000H      ; start from 'X' jump, Basic prompt
         
         ld bc, $2000    ; Break is anything from $2000 to $3FFF
         out (c), A      ; output the 0 to Break to Single Step mode
@@ -26,5 +16,10 @@ OMCR_IOC        .EQU   $20    ; IO Control (1 64180 Mode)
                         ; poke mb+3, &hed
                         ; poke mb+4, &h79
                         ; poke mb+5, &hc9
+
+
+        .org WRKSPC+3H  ; at the USR(0) jump in Basic
+        
+        JP 3000H        ; jump to the BREAK code.
         
         .end
